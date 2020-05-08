719471b2378cf0e87da15bf05a9a7bd5a84e8ed2
pabk <philip.boeken@hotmail.nl>
2020-05-08 14:47:49 +0200

    speedup parallel tests

--- indepTests/CCIT.R
+++ indepTests/CCIT.R
@@ -1,13 +1,13 @@
-suppressWarnings(library(reticulate))
-use_python('/usr/local/bin/python3')
+# suppressWarnings(library(reticulate))
+# use_python('/usr/local/bin/python3')
 
 CCIT <- function (X, Y, Z = NULL) {
-  ccit <- import('CCIT')
-  ccit <- ccit$CCIT$CCIT
+  # .ccit <- import('CCIT')
+  # .ccit <- .ccit$CCIT$CCIT
   
   if (is.null(Z)){
-    return(suppressWarnings(ccit(matrix(X, ncol=1), matrix(Y, ncol=1), NULL)))
+    return(suppressWarnings(.ccit(matrix(X, ncol=1), matrix(Y, ncol=1), NULL)))
   }
   
-  return(suppressWarnings(ccit(matrix(X, ncol=1), matrix(Y, ncol=1), matrix(Z, ncol=1))))
+  return(suppressWarnings(.ccit(matrix(X, ncol=1), matrix(Y, ncol=1), matrix(Z, ncol=1))))
 }
--- init.R
+++ init.R
@@ -1,5 +1,4 @@
 # source conditional independence tests
-source('indepTests/parcortest.R')
 source('indepTests/splineGCM.R')
 source('indepTests/bayesCITest.R')
 source('indepTests/CCIT.R')
@@ -8,3 +7,7 @@ source('helpers.R')
 
 suppressWarnings(library(ppcor))
 suppressWarnings(library(RCIT))
+suppressWarnings(library(reticulate))
+use_python('/usr/local/bin/python3')
+.ccit <- import('CCIT')
+.ccit <- ccit$CCIT$CCIT
