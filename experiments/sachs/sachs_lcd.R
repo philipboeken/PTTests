760c757b4638881121e048bdd63bceec2e174c34
pabk <philip.boeken@hotmail.nl>
2020-05-11 14:24:30 +0200

    minor improvements

--- experiments/sachs/sachs_lcd.R
+++ experiments/sachs/sachs_lcd.R
@@ -1,3 +1,7 @@
+# Clear workspace
+rm(list = ls(all.names = TRUE))
+
+# Imports
 source('independence_tests/test_wrappers.R')
 source('helpers.R')
 suppressWarnings(library(foreach))
@@ -28,9 +32,9 @@ sachs_data_pooled <- read_csv("experiments/sachs/sachs_data_pooled.csv",
                               col_types = .col_types)
 
 .context_vars <- c('AKT inh', 'G0076', 'LY294002', 'PMA/beta2CAMP + noAlphaCD3/28', 
-                      'Psitectorigenin', 'U0126', 'experiment')
+                   'Psitectorigenin', 'U0126')
 
-.system_vars <- setdiff(colnames(sachs_data_pooled), .context_vars)
+.system_vars <- setdiff(colnames(sachs_data_pooled), c(.context_vars, 'experiment'))
 
 pos_lcd_triples <- as.matrix(expand.grid(C=.context_vars, X=.system_vars, Y=.system_vars))
 pos_lcd_triples <- pos_lcd_triples[which(pos_lcd_triples[ ,2] != pos_lcd_triples[ ,3]), ]
@@ -38,11 +42,12 @@ pos_lcd_triples <- pos_lcd_triples[which(pos_lcd_triples[ ,2] != pos_lcd_triples
 get_results <- function(test) {
   result <- foreach(i=1:nrow(pos_lcd_triples), .combine=rbind) %dopar% {
     lcd_triple <- pos_lcd_triples[i,]
-    data <- as.matrix(sachs_data_pooled[, lcd_triple])
+    idx <- which(sachs_data_pooled[,lcd_triple[1]] == 1 | sachs_data_pooled[,'experiment'] == 1)
+    data <- as.matrix(sachs_data_pooled[idx, lcd_triple])
     ts <- test(data[,1], data[,2])
     uci <- test(data[,2], data[,3])
     ci <- test(data[,1], data[,2], data[,3])
-    lcd <- (1 - ts) * (1 - uci) * ci
+    lcd <- min((1 - ts), (1 - uci), ci)
     return(data.frame(ts=ts, uci=uci, ci=ci, lcd=lcd))
   }
   return(result)
