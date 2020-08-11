5fd59071fa9eb63f240bfc47d342f976733461fd
pabk <philip.boeken@hotmail.nl>
2020-08-11 11:07:45 +0200

    update

--- R/experiment_lcd_compare_tests.R
+++ R/experiment_lcd_compare_tests.R
@@ -1,32 +1,32 @@
-experiment_lcd_compare_tests <- function (m = 1000, n = 400, graph_probs = c(3/5, 1/5, 1/5), 
-                                          dim_C = 2, err_sd = 1/2, p_link = 4/5, 
+experiment_lcd_compare_tests <- function(m = 1000, n = 400, graph_probs = c(3 / 5, 1 / 5, 1 / 5),
+                                          dim_C = 2, err_sd = 1 / 2, p_link = 4 / 5,
                                           interv_options = c(mean_shift, variance_shift, mixture),
                                           nonlin_options = c(linear, parabolic, sinusoidal),
                                           seed = 0,
                                           path = 'output/lcd_compare_tests/',
                                           save_figures = TRUE) {
-  
+
   # Setup test
   ##############################################
   set.seed(seed)
-  
-  get_results <- function(dataset, test){
+
+  get_results <- function(dataset, test) {
     `%dopar%` <- foreach::`%dopar%`
     result <- foreach::foreach(i = 1:length(dataset), .combine = rbind) %dopar% {
       data <- dataset[[i]]
-      
+
       start_time_CX <- Sys.time()
       CX <- test(data$C, data$X)
       end_time_CX <- Sys.time()
-      
+
       start_time_XY <- Sys.time()
       XY <- test(data$X, data$Y)
       end_time_XY <- Sys.time()
-      
+
       start_time_CY_X <- Sys.time()
       CY_X <- test(data$C, data$Y, data$X)
       end_time_CY_X <- Sys.time()
-      
+
       return(data.frame(
         label_CX = data$label_CX,
         label_XY = data$label_XY,
@@ -40,68 +40,68 @@ experiment_lcd_compare_tests <- function (m = 1000, n = 400, graph_probs = c(3/5
     }
     return(result)
   }
-  
+
   # Do test
   ##############################################
-  
+
   cores <- parallel::detectCores()
-  cl <-parallel:: makeForkCluster(cores[1]-1)
+  cl <- parallel::makeForkCluster(cores[1] - 1)
   doParallel::registerDoParallel(cl)
   # doParallel::registerDoParallel()
-  
-  data <- lapply(1:m, function (i) get_data(graph_probs, n, dim_C, err_sd, p_link, 
+
+  data <- lapply(1:m, function(i) get_data(graph_probs, n, dim_C, err_sd, p_link,
                                             interv_options, nonlin_options))
-  
+
   results <- list(
     ppcor = get_results(data, .ppcor_wrapper),
-    # spcor = get_results(data, .spcor_wrapper),
-    # ppcor_b = get_results(data, .ppcor_b_wrapper),
-    # gcm = get_results(data, .gcm_wrapper),
+  # spcor = get_results(data, .spcor_wrapper),
+  # ppcor_b = get_results(data, .ppcor_b_wrapper),
+  # gcm = get_results(data, .gcm_wrapper),
     rcot = get_results(data, .rcot_wrapper),
-    # ccit = get_results(data, .ccit_wrapper),
+  # ccit = get_results(data, .ccit_wrapper),
     polyatree = get_results(data, .polyatree_wrapper),
     polyatree_c = get_results(data, .polyatree_wrapper_continuous)
   )
-  
-  
+
+
   # Process results
   ##############################################
-  
-  .get_results_by_type <- function (results, type) {
-    labels <- results$polyatree[,{{paste('label_',type, sep = '')}}]
+
+  .get_results_by_type <- function(results, type) {
+    labels <- results$polyatree[, {{ paste('label_', type, sep = '') }}]
     labels <- factor(labels, ordered = TRUE, levels = c(1, 0))
     result <- data.frame(label = labels)
     for (test in names(results)) {
-      result[test] <- results[[test]][,type]
+      result[test] <- results[[test]][, type]
     }
     return(result)
   }
-  
+
   `%do%` <- foreach::`%do%`
   times <- foreach::foreach(test = names(results), .combine = rbind) %do% {
-    rbind(c(test, '1_CX', sum(results[[test]][,'time_CX'])),
-          c(test, '2_XY', sum(results[[test]][,'time_XY'])),
-          c(test, '3_CY_X', sum(results[[test]][,'time_CY_X'])))
+    rbind(c(test, '1_CX', sum(results[[test]][, 'time_CX'])),
+          c(test, '2_XY', sum(results[[test]][, 'time_XY'])),
+          c(test, '3_CY_X', sum(results[[test]][, 'time_CY_X'])))
   }
-  times <- data.frame(ensemble = times[,1], test = times[,2], time = as.double(times[,3]))
-  
+  times <- data.frame(ensemble = times[, 1], test = times[, 2], time = as.double(times[, 3]))
+
   CX_results <- .get_results_by_type(results, 'CX')
   XY_results <- .get_results_by_type(results, 'XY')
   CY_X_results <- .get_results_by_type(results, 'CY_X')
-  
-  labels_lcd <- factor(results$polyatree[,'label_lcd'], ordered = TRUE, levels = c(1,0))
-  
-  plot_CX <- .plot_roc(CX_results[,1], CX_results[,-1], freq_default = 0.05)
-  plot_XY <- .plot_roc(XY_results[,1], XY_results[,-1], freq_default = 0.05)
-  plot_CY_X <- .plot_roc(CY_X_results[,1], CY_X_results[,-1], freq_default = 0.05)
-  plot_lcd <- .plot_roc_custom(labels_lcd, CX_results[,-1], XY_results[,-1], CY_X_results[,-1])
-  
+
+  labels_lcd <- factor(results$polyatree[, 'label_lcd'], ordered = TRUE, levels = c(1, 0))
+
+  plot_CX <- .plot_roc(CX_results[, 1], CX_results[, -1], freq_default = 0.05)
+  plot_XY <- .plot_roc(XY_results[, 1], XY_results[, -1], freq_default = 0.05)
+  plot_CY_X <- .plot_roc(CY_X_results[, 1], CY_X_results[, -1], freq_default = 0.05)
+  plot_lcd <- .plot_roc_custom(labels_lcd, CX_results[, -1], XY_results[, -1], CY_X_results[, -1])
+
   grid <- cowplot::plot_grid(plot_CX, plot_XY, plot_CY_X, plot_lcd, nrow = 1)
-  
+
   plot_runtimes <- .plot_times(times)
-  
+
   timestamp <- format(Sys.time(), '%Y%m%d_%H%M%S')
-  
+
   save(results, file = sprintf("%s%s.Rdata", path, timestamp))
   .ggsave(paste(path, timestamp, sep = ''), grid, 40, 10)
   .ggsave(paste(path, 'last', sep = ''), grid, 40, 10)
