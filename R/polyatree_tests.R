f99fb6e18187cbb8e41ee4d3b30629e1f1675709
pabk <philip.boeken@hotmail.nl>
2020-10-06 13:30:02 +0200

    add option for returning the log(BF)

--- R/polyatree_tests.R
+++ R/polyatree_tests.R
@@ -1,24 +1,22 @@
 pt_ci_test <- function(X, Y, Z = NULL, rho = 0.5, c = 1,
-                              max_depth = -1, qdist = qnorm, verbose = TRUE) {
+                       max_depth = -1, qdist = qnorm,
+                       verbose = TRUE, log_BF = FALSE) {
   if (is.null(Z)) {
-    if (verbose)
-      cat('Redirecting to independence test\n')
-    return(pt_independence_test(X, Y, c, max_depth, qdist, verbose))
+    if (verbose) cat('Redirecting to independence test\n')
+    return(pt_independence_test(X, Y, c, max_depth, qdist, verbose, log_BF))
   }
 
   if (.is_discrete(X) || .is_discrete(Y)) {
-    if (verbose)
-      cat('Performing conditional two-sample test\n')
-    return(pt_d_sample_ci_test(X, Y, Z, rho, c, max_depth, qdist))
+    if (verbose) cat('Performing conditional two-sample test\n')
+    return(pt_d_sample_ci_test(X, Y, Z, rho, c, max_depth, qdist, log_BF))
   }
 
-  if (verbose)
-    cat('Performing continuous conditional independence test\n')
-  return(pt_continuous_ci_test(X, Y, Z, rho, c, max_depth, qdist))
+  if (verbose) cat('Performing continuous conditional independence test\n')
+  return(pt_continuous_ci_test(X, Y, Z, rho, c, max_depth, qdist, log_BF))
 }
 
 pt_d_sample_ci_test <- function(X, Y, Z, rho = 0.5, c = 1,
-                                       max_depth = -1, qdist = qnorm) {
+                                max_depth = -1, qdist = qnorm, log_BF = FALSE) {
   old_expressions <- options()$expressions
   options(expressions = max(max_depth, old_expressions))
 
@@ -54,10 +52,14 @@ pt_d_sample_ci_test <- function(X, Y, Z, rho = 0.5, c = 1,
   }))
 
   n_hypotheses <- length(discrete_values)
-  bf <- exp(p_H0 - p_H1) * n_hypotheses # Bayes Factor with a Bonferroni-type correction
+  bf <- p_H0 - p_H1 + log(n_hypotheses) # Bayes Factor with a Bonferroni-type correction
 
   options(expressions = old_expressions)
 
+  if (log_BF)
+    return(list(bf = bf, p_H0 = NULL, p_H1 = NULL))
+
+  bf <- exp(bf)
   return(list(bf = bf, p_H0 = 1 - 1 / (1 + bf), p_H1 = 1 / (1 + bf)))
 }
 
@@ -66,7 +68,7 @@ pt_d_sample_ci_test <- function(X, Y, Z, rho = 0.5, c = 1,
 # nonparametric test for conditional independence.
 # https://arxiv.org/abs/1910.11219
 pt_continuous_ci_test <- function(X, Y, Z = NULL, rho = 0.5, c = 1,
-                                         max_depth = -1, qdist = qnorm) {
+                                  max_depth = -1, qdist = qnorm, log_BF = FALSE) {
   old_expressions <- options()$expressions
   options(expressions = max(max_depth, old_expressions))
 
@@ -87,10 +89,14 @@ pt_continuous_ci_test <- function(X, Y, Z = NULL, rho = 0.5, c = 1,
                                              z_min = 0, z_max = 1,
                                              c = 1 * c, rho = rho, depth = 1, max_depth, qdist)
 
-  bf <- exp(phi_xy_indep - phi_xy_dep)
+  bf <- phi_xy_indep - phi_xy_dep
 
   options(expressions = old_expressions)
 
+  if (log_BF)
+    return(list(bf = bf, p_H0 = NULL, p_H1 = NULL))
+
+  bf <- exp(bf)
   return(list(bf = bf, p_H0 = 1 - 1 / (1 + bf), p_H1 = 1 / (1 + bf)))
 }
 
@@ -114,23 +120,23 @@ pt_continuous_ci_test <- function(X, Y, Z = NULL, rho = 0.5, c = 1,
   return(matrixStats::logSumExp(c(log(rho) + logl, log(1 - rho) + phi_1 + phi_2)))
 }
 
-pt_independence_test <- function(X, Y, c = 1, max_depth = -1, qdist = qnorm, verbose = TRUE) {
+pt_independence_test <- function(X, Y, c = 1, max_depth = -1, qdist = qnorm, verbose = TRUE, log_BF = FALSE) {
   if (.is_discrete(X) || .is_discrete(Y)) {
     if (verbose)
       cat('Performing two-sample test\n')
-    return(pt_d_sample_test(X, Y, c = c, max_depth = max_depth, qdist = qdist))
+    return(pt_d_sample_test(X, Y, c = c, max_depth = max_depth, qdist = qdist, log_BF))
   }
 
   if (verbose)
     cat('Performing continuous independence test\n')
-  return(pt_continuous_independence_test(X, Y, c = c, max_depth = max_depth, qdist = qdist))
+  return(pt_continuous_independence_test(X, Y, c = c, max_depth = max_depth, qdist = qdist, log_BF))
 }
 
 # Implements a continuous independence test.
 # See Filippi, S. and Holmes, C. C. (2017). A Bayesian 
 # nonparametric approach to testing for dependence 
 # between random variables. Bayesian Analysis, 12(4):919–938.
-pt_continuous_independence_test <- function(X, Y, c = 1, max_depth = -1, qdist = qnorm) {
+pt_continuous_independence_test <- function(X, Y, c = 1, max_depth = -1, qdist = qnorm, log_BF = FALSE) {
   old_expressions <- options()$expressions
   options(expressions = max(max_depth, old_expressions))
 
@@ -147,10 +153,14 @@ pt_continuous_independence_test <- function(X, Y, c = 1, max_depth = -1, qdist =
   p_xy <- .pt_marginal_likelihood(XY, low = c(0, 0), up = c(1, 1), c = 1 * c,
                                          depth = 1, max_depth, qdist)
 
-  bf <- exp(p_x + p_y - p_xy)
+  bf <- p_x + p_y - p_xy
 
   options(expressions = old_expressions)
 
+  if (log_BF)
+    return(list(bf = bf, p_H0 = NULL, p_H1 = NULL))
+
+  bf <- exp(bf)
   return(list(bf = bf, p_H0 = 1 - 1 / (1 + bf), p_H1 = 1 / (1 + bf)))
 }
 
@@ -158,7 +168,7 @@ pt_continuous_independence_test <- function(X, Y, c = 1, max_depth = -1, qdist =
 # Holmes, C. C., Caron, F., Griffin, J. E., and Stephens, D. A. (2015).
 # Two-sample Bayesian nonparametric hypothesis testing.
 # Bayesian Analysis, 10(2):297–320.
-pt_d_sample_test <- function(X, Y, c = 1, max_depth = -1, qdist = qnorm) {
+pt_d_sample_test <- function(X, Y, c = 1, max_depth = -1, qdist = qnorm, log_BF = FALSE) {
   old_expressions <- options()$expressions
   options(expressions = max(max_depth, old_expressions))
 
@@ -185,10 +195,14 @@ pt_d_sample_test <- function(X, Y, c = 1, max_depth = -1, qdist = qnorm) {
   }))
 
   n_hypotheses <- length(discrete_values)
-  bf <- exp(p_H0 - p_H1) * n_hypotheses # Bayes Factor with a Bonferroni-type correction
+  bf <- p_H0 - p_H1 + log(n_hypotheses) # Bayes Factor with a Bonferroni-type correction
 
   options(expressions = old_expressions)
 
+  if (log_BF)
+    return(list(bf = bf, p_H0 = NULL, p_H1 = NULL))
+
+  bf <- exp(bf)
   return(list(bf = bf, p_H0 = 1 - 1 / (1 + bf), p_H1 = 1 / (1 + bf)))
 }
 
