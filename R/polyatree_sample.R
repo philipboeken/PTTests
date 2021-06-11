eea959c49cbac9592320fe7ef662b322d19ee1a1
pabk <philip.boeken@hotmail.nl>
2021-06-11 15:54:57 +0200

    fix bug

--- R/polyatree_sample.R
+++ R/polyatree_sample.R
@@ -13,15 +13,14 @@ polyatree_sample <- function(c = 1, depth = 12, aj = function(j) c*j^2, save = F
     ps[[j+1]] <- ps[[j+1]] * as.vector(t(theta))
   }
   
-  barplot(ps[[depth]])
   qs <- qnorm((1:(2**depth) - 0.5) / (2**depth))
   tmp <- qnorm((0:(2**depth)) / (2**depth))
   widths <- tmp[2:length(tmp)] - tmp[1:(length(tmp)-1)]
   y <- ps[[depth+1]] / widths
-  plot(qs, ps[[depth+1]] / widths, type='l')
   
-  plt <- ggplot2::ggplot(data=data.frame(x=qs, y=y), aes(x=x, y=y, group=1)) + geom_line() +
-    theme(axis.title.x=element_blank(), axis.title.y=element_blank())
+  plt <- ggplot2::ggplot(data=data.frame(x=qs, y=y), ggplot2::aes(x=x, y=y, group=1)) +
+    ggplot2::geom_line() +
+    ggplot2::theme(axis.title.x=ggplot2::element_blank(), axis.title.y=ggplot2::element_blank())
   if(save) {
     .ggsave('output/polyatree_sample', plt, 8, 8)
   }
